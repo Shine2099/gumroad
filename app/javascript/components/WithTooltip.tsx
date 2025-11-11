a1efb08adb6eac99d7c03dec0e1ec223e36aa7a8
Maya <19721695+code-elf@users.noreply.github.com>
2025-11-11 23:01:10 +0100

    fix

--- app/javascript/components/WithTooltip.tsx
+++ app/javascript/components/WithTooltip.tsx
@@ -4,6 +4,11 @@ import { classNames } from "$app/utils/classNames";
 
 export type Position = "top" | "left" | "bottom" | "right";
 
+const centerClasses = (position: Position) => ({
+  "left-1/2 -translate-x-1/2": position === "top" || position === "bottom",
+  "top-1/2 -translate-y-1/2": position === "left" || position === "right",
+});
+
 type Props = {
   children: React.ReactNode;
   tip: React.ReactNode | null;
@@ -25,21 +30,22 @@ export const WithTooltip = ({ tip, children, position = "bottom", className, too
           {...tooltipProps}
           className={classNames(
             "absolute z-30 hidden w-40 max-w-max rounded-md bg-primary p-3 text-primary-foreground group-hover:block",
+            centerClasses(position),
             {
-              "bottom-full left-1/2 -translate-x-1/2 -translate-y-2": position === "top",
-              "top-1/2 right-full -translate-x-2 -translate-y-1/2": position === "left",
-              "top-full left-1/2 -translate-x-1/2 translate-y-2": position === "bottom",
-              "top-1/2 left-full translate-x-2 -translate-y-1/2": position === "right",
+              "bottom-full -translate-y-2": position === "top",
+              "right-full -translate-x-2": position === "left",
+              "top-full translate-y-2": position === "bottom",
+              "left-full translate-x-2": position === "right",
             },
             tooltipProps?.className,
           )}
         >
           <div
-            className={classNames("absolute border-6 border-transparent", {
-              "top-full left-1/2 -translate-x-1/2 border-t-primary": position === "top",
-              "top-1/2 left-full -translate-y-1/2 border-l-primary": position === "left",
-              "bottom-full left-1/2 -translate-x-1/2 border-b-primary": position === "bottom",
-              "top-1/2 right-full -translate-y-1/2 border-r-primary": position === "right",
+            className={classNames("absolute border-6 border-transparent", centerClasses(position), {
+              "top-full border-t-primary": position === "top",
+              "left-full border-l-primary": position === "left",
+              "bottom-full border-b-primary": position === "bottom",
+              "right-full border-r-primary": position === "right",
             })}
           ></div>
           {tip}
