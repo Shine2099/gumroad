26ab085cdbe363b4fa47bfa07dd9040af62b089c
Salman Najah <salmannajah123@gmail.com>
2025-12-11 20:40:56 +0530

    icon spec

--- spec/helpers/icon_helper_spec.rb
+++ spec/helpers/icon_helper_spec.rb
@@ -6,24 +6,33 @@ describe IconHelper do
   describe "#icon" do
     it "renders icon without options" do
       output = icon("solid-search")
-      expect(output).to eq('<span class="icon icon-solid-search"></span>')
+      expect(output).to include("mask-[url(~images/icons/solid-search.svg)]")
+      expect(output).to include("bg-current")
     end
 
     it "renders icon with options" do
       output = icon("solid-search", class: "warning", title: "Search")
-      expect(output).to eq('<span class="icon icon-solid-search warning" title="Search"></span>')
+      expect(output).to include("mask-[url(~images/icons/solid-search.svg)]")
+      expect(output).to include("warning")
+      expect(output).to include('title="Search"')
     end
   end
 
   describe "#icon_yes" do
     it "renders the icon" do
-      expect(icon_yes).to eq('<span aria-label="Yes" style="color: rgb(var(--success))" class="icon icon-solid-check-circle"></span>')
+      output = icon_yes
+      expect(output).to include("mask-[url(~images/icons/solid-check-circle.svg)]")
+      expect(output).to include('aria-label="Yes"')
+      expect(output).to include('style="color: rgb(var(--success))"')
     end
   end
 
   describe "#icon_no" do
     it "renders the icon" do
-      expect(icon_no).to eq('<span aria-label="No" style="color: rgb(var(--danger))" class="icon icon-x-circle-fill"></span>')
+      output = icon_no
+      expect(output).to include("mask-[url(~images/icons/x-circle-fill.svg)]")
+      expect(output).to include('aria-label="No"')
+      expect(output).to include('style="color: rgb(var(--danger))"')
     end
   end
 end
