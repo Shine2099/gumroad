1131dba51cdcf3cda63527578f953aad2370d0a1
Aditya Garud <garudaditya079@gmail.com>
2026-01-15 11:34:56 +0530

    ref

--- app/controllers/help_center/articles_controller.rb
+++ app/controllers/help_center/articles_controller.rb
@@ -4,11 +4,13 @@ class HelpCenter::ArticlesController < HelpCenter::BaseController
   before_action :redirect_legacy_articles, only: :show
 
   def index
+    @title = "Gumroad Help Center"
     render inertia: "HelpCenter/Index", props: help_center_presenter.index_props
   end
 
   def show
     article = HelpCenter::Article.find_by!(slug: params[:slug])
+    @title = "#{article.title} - Gumroad Help Center"
     render inertia: "HelpCenter/Article/Show", props: help_center_presenter.article_props(article)
   end
 
