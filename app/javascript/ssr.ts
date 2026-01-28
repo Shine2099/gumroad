75c8ff3df59dba937b6b8fd5d9b62a1edc82eacb
alina-alyy <Alina.ali2712@gmail.com>
2026-01-28 10:57:42 +0500

    fix: merge conflict

--- app/javascript/ssr.ts
+++ app/javascript/ssr.ts
@@ -16,7 +16,6 @@ import DownloadPageWithContent from "$app/components/server-components/DownloadP
 import DownloadPageWithoutContent from "$app/components/server-components/DownloadPage/WithoutContent";
 import GenerateInvoiceConfirmationPage from "$app/components/server-components/GenerateInvoiceConfirmationPage";
 import GenerateInvoicePage from "$app/components/server-components/GenerateInvoicePage";
-import HelpCenterArticlesIndexPage from "$app/components/server-components/HelpCenter/ArticlesIndexPage";
 import Nav from "$app/components/server-components/Nav";
 import PdfReaderPage from "$app/components/server-components/PdfReaderPage";
 import ProductPage from "$app/components/server-components/Product";
