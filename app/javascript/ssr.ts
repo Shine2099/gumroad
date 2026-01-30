e506e630e5bb57e30a813e93ca5bf97f8f534fd7
alina-alyy <Alina.ali2712@gmail.com>
2026-01-30 23:00:51 +0500

    Merge upstream/main into migrate-secure-redirect-page

--- app/javascript/ssr.ts
+++ app/javascript/ssr.ts
@@ -26,7 +26,6 @@ import ProfileCoffeePage from "$app/components/server-components/Profile/CoffeeP
 import ProfileProductPage from "$app/components/server-components/Profile/ProductPage";
 import ProfileWishlistPage from "$app/components/server-components/Profile/WishlistPage";
 import DisputeEvidencePage from "$app/components/server-components/Purchase/DisputeEvidencePage";
-import PurchaseProductPage from "$app/components/server-components/Purchase/ProductPage";
 import SubscribePage from "$app/components/server-components/SubscribePage";
 import SubscriptionManager from "$app/components/server-components/SubscriptionManager";
 import SubscriptionManagerMagicLink from "$app/components/server-components/SubscriptionManagerMagicLink";
